0a04022eece8fb6ea241baf4a2e9fda17563ca22
lktop <1277843354@qq.com>
2019-10-16 17:26:36 +0800

    项目结构更新！

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app(config_filename):
+    app = Flask(__name__)
+    app.config.from_object(config_filename)
+
+    db.init_app(app)
+
+    # 用户模块
+    from app.users.api import init_api
+    init_api(app)
+
+    return app
